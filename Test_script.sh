3a803562f068e14c3c8b744290ee9841e3e71c71
Saurabh-Kumar-Gupta <saurabh.gupta.6174@gmail.com>
2022-01-21 12:23:09 +0530

    Update Test_script.sh
    additional option for software installation added

--- Test_script.sh
+++ Test_script.sh
@@ -1,14 +1,27 @@
 echo " This script is purely expiermental, run it at your own risk "
 read -p "Press any key to continue . . ."; echo
 ins(){
-    for program in ${list[@]}; do
-        sudo apt install $program -y
-        echo "$program installed succcesfully"
-    done    
-}
-list=(vlc telegram-desktop ranger neovim firefox figlet ubuntu-restricted-extras gnome-tweaks gparted htop neofetch qbittorrent mpv ifconfig stacer)
-ins list
-sudo apt update && sudo apt upgrade -y
+         sudo apt install $program -y
+         echo "$program installed succcesfully"
+     done    
+ }
+
+list=(vlc telegram-desktop ranger neovim firefox figlet ubuntu-restricted-extras gnome-tweaks gparted htop neofetch qbittorrent mpv stacer)
+l = ${#list[@]}
+echo $l
+ins
+
+echo " if you want to install any other software please enter 0 otherwise enter 1"
+DEFAULT=0
+read INPUT
+if [ "$INPUT" -eq "$DEFAULT" ]
+then
+     read INPUT2	
+     ins INPUT2;
+else
+    echo "No Value Found";
+fi
+# sudo apt update && sudo apt upgrade -y
 
  
 #echo 'figlet shoo' >> ~/.bashrc
