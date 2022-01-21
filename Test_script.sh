48d038aa373e15a3325b3d80c4cccff41b591659
Saurabh-Kumar-Gupta <saurabh.gupta.6174@gmail.com>
2022-01-21 20:39:38 +0530

    a more intuitive way to make the function call

--- Test_script.sh
+++ Test_script.sh
@@ -1,28 +1,26 @@
 echo " This script is purely expiermental, run it at your own risk "
 read -p "Press any key to continue . . ."; echo
 ins(){
-     for program in ${list[@]}; do
-         sudo apt install $program -y
-         echo "$program installed succcesfully"
-     done    
+    sudo apt install $1 -y
+    echo "$1 installed succcesfully"    
  }
 
-list=(vlc telegram-desktop ranger neovim firefox figlet ubuntu-restricted-extras gnome-tweaks gparted htop neofetch qbittorrent mpv stacer)
-l = ${#list[@]}
-echo $l
-ins
-
+declare -a list=(vlc telegram-desktop ranger neovim firefox figlet ubuntu-restricted-extras gnome-tweaks gparted htop neofetch qbittorrent mpv stacer)
+l=${#list[@]}
+for((i=0;i<$[l]; i++));
+do
+   ins ${list[$i]}
+done   
 echo " if you want to install any other software please enter 0 otherwise enter 1"
 DEFAULT=0
 read INPUT
 if [ "$INPUT" -eq "$DEFAULT" ]
 then
      read INPUT2	
-     ins INPUT2;
+     ins $INPUT2;
 else
     echo "No Value Found";
 fi
-# sudo apt update && sudo apt upgrade -y
 
  
 #echo 'figlet shoo' >> ~/.bashrc
