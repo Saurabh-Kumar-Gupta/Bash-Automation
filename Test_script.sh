380ec6d5d3bfddd0400b986ba22e2e12e7f95bfa
Saurabh-Kumar-Gupta <saurabh.gupta.6174@gmail.com>
2022-02-01 23:52:30 +0530

    wallpaper set

--- Test_script.sh
+++ Test_script.sh
@@ -35,6 +35,10 @@ do
    fi
 done
 
+wget -O /tmp/wallpaper.jpg "http://www.bing.com/$(wget -q -O- https://binged.it/2ZButYc | sed -e 's/<[^>]*>//g' | cut -d / -f2 | cut -d \& -f1)"
+gsettings set org.gnome.desktop.background picture-uri file:////tmp/wallpaper.jpg
+
+
 # ---------------------------aliashes and effects------------------------------------------
 
 echo "# user edited effects" >> ~/.bashrc
