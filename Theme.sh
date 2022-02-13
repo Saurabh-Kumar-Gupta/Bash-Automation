4cf45a3f884ff4e685be6a3b8aba4585df54e56c
Saurabh-Kumar-Gupta <saurabh.gupta.6174@gmail.com>
2022-02-13 19:24:18 +0530

    added juno themes

--- Theme.sh
+++ Theme.sh
@@ -4,3 +4,9 @@
 # wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Nordic-darker-v40.tar.xz
 # tar -xvf Nordic-darker-v40.tar.xz
 # mv Nordic-darker-v40 ~/.themes
+wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Juno-ocean-v40.tar.xz
+tar -xvf Juno-ocean-v40.tar.xz
+mv Juno-ocean-v40 ~/.themes
+wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Juno-palenight-v40.tar.xz
+tar -xvf Juno-palenight-v40.tar.xz
+mv Juno-palenight-v40 ~/.themes
