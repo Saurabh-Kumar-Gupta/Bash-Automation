fed13739db42e64f1d589c19b53376316c60c917
Saurabh-Kumar-Gupta <saurabh.gupta.6174@gmail.com>
2022-02-13 19:03:48 +0530

    Added Second Theme

--- Theme.sh
+++ Theme.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-wget https://github.com/dracula/gtk/archive/master.zip
-unzip -o master.zip -d ~/.themes
+# wget https://github.com/dracula/gtk/archive/master.zip
+# unzip -o master.zip -d ~/.themes
+# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Nordic-darker-v40.tar.xz
+# tar -xvf Nordic-darker-v40.tar.xz
+# mv Nordic-darker-v40 ~/.themes
