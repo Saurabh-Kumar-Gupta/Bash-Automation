be87cca54b9a321ea698dbeef0e078e053eaa614
Saurabh-Kumar-Gupta <saurabh.gupta.6174@gmail.com>
2022-02-13 19:32:14 +0530

    added kimi

--- Theme.sh
+++ Theme.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # wget https://github.com/dracula/gtk/archive/master.zip
 # unzip -o master.zip -d ~/.themes
+
+# Nordic 
 # wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Nordic-darker-v40.tar.xz
 # tar -xvf Nordic-darker-v40.tar.xz
 # mv Nordic-darker-v40 ~/.themes
+
+# Juno -2 themes
 wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Juno-ocean-v40.tar.xz
 tar -xvf Juno-ocean-v40.tar.xz
 mv Juno-ocean-v40 ~/.themes
