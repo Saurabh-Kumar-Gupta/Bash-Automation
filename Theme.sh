dc7e522ba4ce0bb6931d329c4640a20915bc1df5
Saurabh-Kumar-Gupta <saurabh.gupta.6174@gmail.com>
2022-02-13 19:50:44 +0530

    .

--- Theme.sh
+++ Theme.sh
@@ -2,15 +2,35 @@
 # wget https://github.com/dracula/gtk/archive/master.zip
 # unzip -o master.zip -d ~/.themes
 
+
+
 # Nordic 
-# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Nordic-darker-v40.tar.xz
+# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Nordic-darker-v40.tar.xz
 # tar -xvf Nordic-darker-v40.tar.xz
 # mv Nordic-darker-v40 ~/.themes
 
-# Juno -2 themes
-wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Juno-ocean-v40.tar.xz
-tar -xvf Juno-ocean-v40.tar.xz
-mv Juno-ocean-v40 ~/.themes
-wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Juno-palenight-v40.tar.xz
-tar -xvf Juno-palenight-v40.tar.xz
-mv Juno-palenight-v40 ~/.themes
+
+
+# Juno -1 themes
+# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Juno-ocean-v40.tar.xz
+# tar -xvf Juno-ocean-v40.tar.xz
+# mv Juno-ocean-v40 ~/.themes
+
+#juno -2 theme
+# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Juno-palenight-v40.tar.xz
+# tar -xvf Juno-palenight-v40.tar.xz
+# mv Juno-palenight-v40 ~/.themes
+
+
+
+# Kimi Theme
+# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Kimi-v40.tar.xz
+# tar -xvf Kimi-v40.tar.xz
+# mv Kimi-v40 ~/.themes
+
+
+
+#Graphite theme -1
+wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Nordic-darker-v40.tar.xz
+tar -xvf Nordic-darker-v40.tar.xz
+mv Nordic-darker-v40 ~/.themes
