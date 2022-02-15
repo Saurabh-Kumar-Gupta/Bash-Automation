cdc06fec7f5fbee301d74b66c24cc0c1ef61f0dd
Saurabh-Kumar-Gupta <saurabh.gupta.6174@gmail.com>
2022-02-15 16:17:36 +0530

    changed into new format

--- Theme.sh
+++ Theme.sh
@@ -16,58 +16,63 @@ ins_theme() {
 
 
 # Nordic 
-# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Nordic-darker-v40.tar.xz
-# tar -xvf Nordic-darker-v40.tar.xz
-# mv Nordic-darker-v40 ~/.themes
+link_1=https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Nordic-darker-v40.tar.xz
+
 
 
 
 # Juno -1 themes
-# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Juno-ocean-v40.tar.xz
-# tar -xvf Juno-ocean-v40.tar.xz
-# mv Juno-ocean-v40 ~/.themes
+link_2=https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Juno-ocean-v40.tar.xz
 
 #juno -2 theme
-# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Juno-palenight-v40.tar.xz
-# tar -xvf Juno-palenight-v40.tar.xz
-# mv Juno-palenight-v40 ~/.themes
+link_3=https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Juno-palenight-v40.tar.xz
+
 
 
 
 # Kimi Theme
-# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Kimi-v40.tar.xz
-# tar -xvf Kimi-v40.tar.xz
-# mv Kimi-v40 ~/.themes
+link_4=https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Kimi-v40.tar.xz
 
 
 
 #Graphite theme -1
-# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Graphite-dark-nord.tar.xz
-# tar -xvf Graphite-dark-nord.tar.xz
-# mv Graphite-dark-nord ~/.themes
+link_5=https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Graphite-dark-nord.tar.xz
+
 
 #Graphics theme -2
-# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Graphite-dark.tar.xz
-# tar -xvf Graphite-dark.tar.xz
-# mv Graphite-dark ~/.themes
+link_6=https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Graphite-dark.tar.xz
 
 #Graphite Theme -3 
-# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Graphite-light-nord.tar.xz
-# tar -xvf Graphite-light-nord.tar.xz
-# mv Graphite-light-nord ~/.themes
+link_7=https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Graphite-light-nord.tar.xz
 
 
 
 # # WhiteSur[MAC] Theme 
-# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/WhiteSur-dark.tar.xz
-# tar -xvf WhiteSur-dark.tar.xz
-# mv WhiteSur-dark ~/.themes
+link_8=https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/WhiteSur-dark.tar.xz
 
 # # WhiteSur[MAC] Theme 
-# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/WhiteSur-light.tar.xz
-# tar -xvf WhiteSur-light.tar.xz
-# mv WhiteSur-light ~/.themes
-# rm -rf WhiteSur-light.tar.xz
+link_9=https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/WhiteSur-light.tar.xz
+
+
+# # Layan Dark Theme 
+link_10=https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Layan-dark.tar.xz
+
+# # Layan Light Theme
+link_11=https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Layan-light.tar.xz
+
+
+
+link_Test=https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Layan-light.tar.xz
+ins_theme $link_Test
+
+
+
+
+
+
+
+
+
 # #Icons
 # wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Icons/WhiteSur-red.tar.xz
 # tar -xvf WhiteSur-red.tar.xz
@@ -77,17 +82,5 @@ ins_theme() {
 
 
 
-# # Layan Dark Theme 
-# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Layan-dark.tar.xz
-# tar -xvf Layan-dark.tar.xz
-# mv Layan-dark ~/.themes
-# rm -rf Layan-dark.tar.xz
 
-# # Layan Light Theme
-# wget https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Layan-light.tar.xz
-# tar -xvf Layan-light.tar.xz
-# mv Layan-light ~/.themes
-# rm -rf Layan-light.tar.xz
 
-link=https://github.com/Saurabh-Kumar-Gupta/Bash-Automation/raw/main/Themes/Layan-light.tar.xz
-ins_theme $link
